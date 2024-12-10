de83d2d7aea79385679e8aac187266a0baeb8c7d
Rugved 💻 <140103108+rugveddanej@users.noreply.github.com>
2024-12-10 23:06:31 +0530

    Create say.py

---
+++ commands/say.py
@@ -0,0 +1,10 @@
+def say(message):
+    print(message)
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("Usage: python say.py <message>")
+    else:
+        message = " ".join(sys.argv[1:])
+        say(message)
