77aa08b79949fe6540245f365bc22e85abe501ae
Rugved 💻 <140103108+rugveddanej@users.noreply.github.com>
2024-12-10 23:00:02 +0530

    Update install.py

--- commands/install.py
+++ commands/install.py
@@ -1,7 +1,12 @@
 import os
+import sys
 
 def install(text):
     os.system(f'say "{text}"')
     
 if __name__ == "__main__":
-    install()
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <text>")
+    else:
+        text = sys.argv[1]
+        install(text)
